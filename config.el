ff75987d15b57317cca66786d2742fdff56f69a5
Rafael Oliveira <rafael.leite.oliveira@gmail.com>
2025-12-04 21:21:42 -0500

    init.

---
+++ config.el
@@ -0,0 +1,142 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq doom-font (font-spec :family "Source Code Pro" :size 14)
+      )
+
+;; Make ruby-lsp-ls highest priority
+(after! lsp-mode
+  (setq lsp-disabled-clients
+        '(sorbet-ls rubocop-ls typeprof-ls ruby-ls ruby-lsp)))
+
+
+                                        ; Bind SPC ! to shell-command
+(map! :leader
+      :desc "Shell command" "!" #'shell-command)
+
+
+(setq lsp-ui-doc-enable t)
+(setq lsp-ui-doc-show-with-cursor t)
+(setq lsp-lens-enable t)
+(setq lsp-ui-sideline-enable t)
+(setq lsp-ui-sideline-show-code-actions t)
+(setq lsp-modeline-code-actions-enable t)
+(setq lsp-signature-auto-activate t)
+(setq lsp-signature-render-documentation t)
+
+(global-evil-matchit-mode 1)
+
+
+(after! org
+  (setq org-agenda-files '("~/Dropbox/org/inbox.org"
+                           "~/Dropbox/org/notes.org"
+                           "~/Dropbox/org/agenda.org"
+                           "~/Dropbox/org/focus.org"))
+
+  (setq org-capture-templates '(("t" "Todo [inbox]" entry
+                                 (file+headline "~/Dropbox/org/inbox.org" "Tasks")
+                                 "* TODO %i%?")
+                                ("T" "Tickler" entry
+                                 (file+headline "~/Dropbox/org/notes.org" "Tickler")
+                                 "* %i%? \n %U")))
+
+  (setq org-refile-targets '(("~/Dropbox/org/inbox.org" :maxlevel . 20)
+                             ("~/Dropbox/org/focus.org" :level . 1)
+                             ("~/Dropbox/org/notes.org" :level . 1)
+                             ))
+  
+
+  )
+(after! org-roam
+  (setq org-roam-directory (file-truename "~/Dropbox/notes"))
+  )
+
+;; Enable citre
+(use-package! citre
+  :defer t
+  :config
+  ;; Tell citre where to find tag files. It automatically detects both.
+  (setq citre-project-root-function #'projectile-project-root)
+
+  ;; Prefer ctags for definitions; use gtags when references are requested.
+  (setq citre-use-default-backends t)
+
+  ;; Keybindings (optional)
+  (map! :leader
+        :desc "Citre jump to def" "j s" #'citre-jump
+        :desc "Citre jump to ref" "j r" #'citre-jump-to-reference
+        :desc "Citre update tags" "j u" #'citre-update-project-tags))
